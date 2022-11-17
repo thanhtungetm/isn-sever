473368594acd70e8b77fed6e9e52a50e64959c2e
Tung Le <thanhtungetm@gmail.com>
2022-11-17 15:59:16 +0700

    add database

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const cors = require('cors')
 require('dotenv').config()
 
 const http = require('http');
-// const { Server } = require("socket.io");
-// const socketServer = require('./socket-server')
+const { Server } = require("socket.io");
+const socketServer = require('./socket-server')
 
 const setupRouter = require('./src/routes')
-// const db = require('./src/services/db.service')
+const db = require('./src/services/db.service')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-// db.connect()
+db.connect()
 
 app.use(cors())
 
@@ -39,13 +39,13 @@ app.use(( err, req, res, next) => {
 });
 
 const server = http.createServer(app);
-// const io = new Server(server,{
-//     cors: {
-//       origin: [process.env.CLIENT_URL||"http://localhost:3001", "http://localhost:3001"],
-//       credentials: true
-//     }
-//   });
-// socketServer(io)
+const io = new Server(server,{
+    cors: {
+      origin: [process.env.CLIENT_URL||"http://localhost:3001", "http://localhost:3001"],
+      credentials: true
+    }
+  });
+socketServer(io)
 
 server.listen(port, () => {
     console.log(`Server is running on PORT ${port}`);
